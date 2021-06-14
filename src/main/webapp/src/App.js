297404d0ff520e72ccac1a81dc4a89200c0ddbaa
mkx <mkx4499@163.com>
2021-06-14 21:18:50 +0800

    home, login, register front-end

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { Container, Row, Col } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import NavigationBar from "./components/NavigationBar";
+import Welcome from "./components/Welcome";
+import Home from "./components/Home";
+import AnswerBook from "./components/AnswerBook";
+import Me from "./components/Me";
+import Footer from "./components/Footer";
+import Register from "./components/User/Register";
+import Login from "./components/User/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const marginTop = {
+		marginTop: "20px",
+	};
+
+	return (
+		<Router>
+			<NavigationBar />
+			<Container>
+				<Row>
+					<Col lg={12} style={marginTop}>
+						<Switch>
+							<Route path="/" exact component={Welcome} />
+							<Route path="/home" exact component={Home} />
+							<Route
+								path="/answerbook"
+								exact
+								component={AnswerBook}
+							/>
+							<Route path="/me" exact component={Me} />
+							<Route
+								path="/register"
+								exact
+								component={Register}
+							/>
+							<Route path="/login" exact component={Login} />
+							<Route path="/logout" exact component={Login} />
+						</Switch>
+					</Col>
+				</Row>
+			</Container>
+			<Footer />
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/main/webapp/src/components/AnswerBook.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class AnswerBook extends React.Component {
+	render() {
+		return <div className="text-white">AnswerBook</div>;
+	}
+}
+
+export default AnswerBook;
